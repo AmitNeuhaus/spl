353fc8165f8b947fdae93ddd00f3b6acc0c89649
amit neuhaus <amit@beehero.io>
2021-11-16 15:50:39 +0200

    implemented some of the action in the studio start fucntion

--- src/Studio.cpp
+++ src/Studio.cpp
@@ -1,7 +1,33 @@
 //
 // Created by AMIT Neuhaus on 08/11/2021.
 //
+# include "../include/Studio.h"
+# include "../include/Action.h"
 
-class Studio{
+void Studio::start() {
+    // TODO: parsing the first word in the command line  == action
+    std::cin << "Studio is now open!" << std::cout
+    bool studioIsOpen = true;
+    while (studioIsOpen) {
+        switch (action) {
+            case "open":
+                // TODO: parse customers from command ,and trainer id
+                // create new customers instances list
+                OpenTrainer *openTrainerInstance = new OpenTrainer(id, customerList);
+                openTrainerInstance->act(this)
+                //TODO: think if need to check that act completed successfully.
+                delete openTrainerInstance;
+            case "order":
+                //parse Trainer ID
+                Order *orderInstance = new Order(trainerId);
+                orderInstance->act(this);
+                //TODO: think if need to check that act completed successfully.
+                delete orderInstance;
+            case "closeAll":
+                studioIsOpen = false;
 
-};
+            default:
+                std::cin << "No action recognized" << std::cout
+        }
+    }
+}
