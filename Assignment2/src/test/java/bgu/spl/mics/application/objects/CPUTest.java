4a0d34b94d893b8677a236bca2ce520db45307cb
amit neuhaus <amit@beehero.io>
2021-12-04 20:23:43 +0200

    modified cpu tetss

--- Assignment2/src/test/java/bgu/spl/mics/application/objects/CPUTest.java
+++ Assignment2/src/test/java/bgu/spl/mics/application/objects/CPUTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 class CPUTest {
 
@@ -20,16 +20,26 @@ class CPUTest {
     void insertDB() {
         int dataSize = cpu.getDataSize();
         DataBatch db = new DataBatch();
-        assertFalse(db.isProcessed());
         cpu.insertDB(db);
         assertEquals(cpu.getDataSize(), dataSize + 1);
+        db.setProcessed(true);
+        dataSize = cpu.getDataSize();
+        assertThrows("Should throw exception because db is already processed",Exception.class,()->cpu.insertDB(db));
+        assertEquals(cpu.getDataSize(), dataSize);
     }
 
     @Test
     void sendProcessedDB() {
         int dataSize = cpu.getDataSize();
-        cpu.sendProcessedDB(new DataBatch());
+        DataBatch db = new DataBatch();
+        db.setProcessed(true);
+        cpu.sendProcessedDB(db);
         assertEquals(cpu.getDataSize(), dataSize - 1);
+
+        dataSize = cpu.getDataSize();
+        db.setProcessed(false);
+        assertThrows("Should throw error because sent out DB isnt proceseed", Exception.class,()-> cpu.sendProcessedDB(db));
+        assertEquals(cpu.getDataSize(), dataSize);
     }
 
 }
