f2037c5df8c381311a7332cf2c019cd06a33bc65
amit neuhaus <amit@beehero.io>
2021-12-29 15:57:36 +0200

    Edited server to use bidimessaging protocol implementation

--- Assignment3/Server/src/main/java/bgu/spl/net/api/bidi/BidiMessagingProtocol.java
+++ Assignment3/Server/src/main/java/bgu/spl/net/api/bidi/BidiMessagingProtocol.java
@@ -1,5 +1,7 @@
 package bgu.spl.net.api.bidi;
 
+import bgu.spl.net.api.MessagingProtocol;
+
 public interface BidiMessagingProtocol<T>  {
 	/**
 	 * Used to initiate the current client protocol with it's personal connection ID and the connections implementation
