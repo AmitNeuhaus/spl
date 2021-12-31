95a95f83364c7083ff04dfbb59675f9cff2ae15d
amit neuhaus <amit@beehero.io>
2021-12-31 15:41:37 +0200

    connected the bidi protocol implementation

--- Assignment3/Server/src/main/java/bgu/spl/net/srv/MessagingEncoderDecoderImpl.java
+++ Assignment3/Server/src/main/java/bgu/spl/net/srv/MessagingEncoderDecoderImpl.java
@@ -5,7 +5,7 @@ import bgu.spl.net.api.MessageEncoderDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
-public class MassagingEncoderDecoderImpl implements MessageEncoderDecoder<String> {
+public class MessagingEncoderDecoderImpl implements MessageEncoderDecoder<String> {
 
     private byte[] bytes = new byte[1<<10];
     private int len = 0;
