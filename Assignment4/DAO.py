386ecdeb30e9aae53eb2b98fa93a6ccc7d1ba4f9
TomCooll <tom20101995@gmail.com>
2022-01-11 19:58:09 +0200

    implement DAO add quantity to Hat

--- Assignment4/DAO.py
+++ Assignment4/DAO.py
@@ -1,5 +1,5 @@
+import sqlite3
 class DAO:
-
 
     def __init__(self,dto_type, conn):
         self.conn = conn
@@ -11,21 +11,56 @@ class DAO:
         ins_dict = vars(dto_instance)
         columns_names = ','.join(ins_dict.keys())
         params = ins_dict.values()
-        qmarks = ",".join()
+        qmarks = ",".join(['?']*len(ins_dict))
+
+        stmt = f'''INSERT INTO{self.table_name} ({columns_names}VALUES({qmarks}))'''
+        self.conn.execute(stmt, params)
 
 
 
-    def update(self):
-        pass
 
-    def delete(self):
-        pass
+    def update(self,new_values, conditions):
+        column_names = list(new_values.keys())
+        new_params = list(new_values.values())
+        cond_cols = list(conditions.keys())
+        cond_values = list(conditions.values())
 
-    def find_first(self):
-        pass
-
-    def find_all(self):
-        pass
+        stmt = f'''UPDATE {self.table_name}SET{','.join([col + '= ?' for col in column_names])} WHERE {' AND '.join([col + '= ?' for col in cond_cols])}'''
+        self.conn.execute(stmt,new_params+cond_values)
 
 
 
+
+    def delete(self,**kwargs):
+        column_name = list(kwargs.keys())
+        params = list(kwargs.values)
+
+        stmt = f'''DELETE FROM{self.table_name}WHERE{' AND '.join([col+ '=?' for col in column_name])}'''
+        self.conn.execute(stmt,prams)
+
+
+
+
+    def find_first(self, **kwargs):
+        column_names = list(kwargs.keys())
+        params = list(kwargs.values())
+
+        stmt = f'''SELECT * FROM{self.table_name}WHERE{' AND '.join([col + '=?' for col in column_name])}ORDER BY id ASC LIMIT 1'''
+        c = self.conn.cursor()
+        c.execute(stmt, params)
+        # TODO: add orm to return
+        return c
+
+    def find_all(self, **kwargs):
+        column_names = list(kwargs.keys())
+        params = list(kwargs.values())
+
+        stmt = f'''SELECT * FROM{self.table_name} WHERE{' AND '.join([col + '=?' for col in column_names])}'''
+        c = self.conn.cursor()
+        c.execute(stmt, params)
+
+
+
+
+if __name__ == "__main__":
+    conn = sqlite3.connect("test.db")
